c93c4e83eebee165b664fdcb1d4f2c04126a5e20
JoaoBorchoski <joaoborchoskidev@gmail.com>
2024-03-15 16:33:44 -0300

    feat: create repo

---
+++ aula1.py
@@ -0,0 +1,39 @@
+# 1- Espera o input do usuario e imprime o quadrado do numero inserido
+# user_input = input("Insira um numero inteiro: ")
+# print(int(user_input) ** 2)
+
+
+# 2- Espera o input do usuario e imprime a soma dos numeros inseridos
+# user_input1 = input("Insira um numero inteiro: ")
+# user_input2 = input("Insira outro numero inteiro: ")
+# print(int(user_input1) + int(user_input2))
+
+
+# 3- Espera o ano de nascimento do usuario e imprime a idade que ele tera em 2023
+# user_input = input("Insira seu ano de nascimento: ")
+# print(2023 - int(user_input))
+
+
+# 4- Espera o input do usuario e imprime a media das notas inseridas
+# user_input1 = input("Insira o nome da disciplina: ")
+# user_input2 = input("Insira a nota 1: ")
+# user_input3 = input("Insira a nota 2: ")
+# user_input4 = input("Insira a nota 3: ")
+# user_input5 = input("Insira a nota 4: ")
+# print(
+#     "A disciplina",
+#     user_input1,
+#     "teve a media",
+#     (int(user_input2) + int(user_input3) + int(user_input4) + int(user_input5)) / 4,
+# )
+
+# 5- Espera o input do usuario e imprime o valor total da compra
+# user_input1 = input("Insira o valor do produto: ")
+# user_input2 = input("Insira a quantidade: ")
+# print("O valor total é de", int(user_input1) * int(user_input2))
+
+
+# 6- Espera o input do usuario e imprime o valor total da compra com desconto de 15%
+# user_input1 = input("Insira o valor do produto: ")
+# user_input2 = input("Insira a quantidade: ")
+# print("O valor total é de", (int(user_input1) * int(user_input2)) * 0.85)
---
+++ aula2.py
@@ -0,0 +1,14 @@
+# 1 - Espera o input do usuario e imprime se o numero inserido é par
+# user_input = input("Insira um numero inteiro: ")
+
+# if int(user_input) % 2 == 0:
+#     print("O numero", user_input, "é par")
+
+
+user_input1 = input("Insira seu ano de nascimento: ")
+user_input2 = input("Já fez aniversario esse ano? ")
+
+if user_input1.lower() == "n":
+    print(2023 - int(user_input1))
+else:
+    print(2023 - int(user_input1) - 1)
