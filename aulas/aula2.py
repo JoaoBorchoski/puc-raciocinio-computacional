ecb635e50fe6b9c5a4568b8b6092e080585fb48e
JoaoBorchoski <joaoborchoskidev@gmail.com>
2024-03-15 16:39:25 -0300

    fix: add description

--- aulas/aula2.py
+++ aulas/aula2.py
@@ -7,7 +7,6 @@
 # 2 - Espera o input do usuario e imprime quantos anos ele tem baseado no ano de nascimento e remove um caso ele nao tenha feito aniversario no ano
 # user_input1 = input("Insira seu ano de nascimento: ")
 # user_input2 = input("Já fez aniversario esse ano? ")
-
 # if user_input1.lower() == "n":
 #     print(2023 - int(user_input1))
 # else:
